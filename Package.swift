5d422f16e8e058b22a541c85288e24c842d7f8a4
Gelareh Taban <gtaban@us.ibm.com>
2017-09-22 15:31:53 -0500

    fix the package.swift

--- Package.swift
+++ Package.swift
@@ -3,50 +3,39 @@
 
 import PackageDescription
 
-#if os(Linux) || os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-
+#if os(Linux) || os(macOS) || os(iOS) || os(tvOS)
+    
 var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/gtaban/security.git", from: "0.0.4"),
     .package(url: "https://github.com/gtaban/BlueSocket.git", from: "0.13.0") ]
 var targetDependencies: [Target.Dependency] = [
     .byNameItem(name: "ServerSecurity"),
     .byNameItem(name: "Socket")]
-
+    
 #if os(Linux)
 packageDependencies.append(.package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "0.3.0"))
 targetDependencies.append(.byNameItem(name: "OpenSSL"))
 #endif
-
+    
 let package = Package(
     name: "TLSService",
-
+    
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "TLSService",
-            targets: ["TLSService"]),
+        .library( name: "TLSService", targets: ["TLSService"]),
         ],
-
-    // Dependencies declare other packages that this package depends
+    
     dependencies: packageDependencies,
-
+    
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "TLSService",
-            dependencies: targetDependencies,
-            exclude: ["Certs"]),
-        .testTarget(
-            name: "TLSServiceTests",
-            dependencies: targetDependencies,
-            exclude: ["Certs"]),
+        .target( name: "TLSService", dependencies: ["ServerSecurity"], exclude: ["Certs"]),
+        .testTarget( name: "TLSServiceTests", dependencies: ["TLSService", "Socket"], exclude: ["Certs"]),
         ]
 )
-
+    
 #else
-
+    
 fatalError("Unsupported OS")
-
+    
 #endif
+
 
