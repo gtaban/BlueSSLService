f710ba5c2d601b547e160be65e9cc81f06bd8264
Gelareh Taban <gtaban@us.ibm.com>
2017-09-22 09:22:37 -0500

    Update the Swift4 manifest

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,31 +1,16 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-//
-//  Package.swift
-//  SSLService
-//
-//  Copyright © 2016 IBM. All rights reserved.
-//
-//     Licensed under the Apache License, Version 2.0 (the "License");
-//     you may not use this file except in compliance with the License.
-//     You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-//     Unless required by applicable law or agreed to in writing, software
-//     distributed under the License is distributed on an "AS IS" BASIS,
-//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//     See the License for the specific language governing permissions and
-//     limitations under the License.
-//
-
 import PackageDescription
 
 #if os(Linux) || os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
 
-var packageDependencies: [Package.Dependency] = [.package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "0.12.0")]
-var targetDependencies: [Target.Dependency] = [.byNameItem(name: "Socket")]
+var packageDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/gtaban/security.git", from: "0"),
+    .package(url: "https://github.com/gtaban/BlueSocket.git", from: "0.13.0") ]
+var targetDependencies: [Target.Dependency] = [
+    .byNameItem(name: "ServerSecurity"),
+    .byNameItem(name: "Socket")]
 
 #if os(Linux)
 packageDependencies.append(.package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "0.3.0"))
@@ -33,27 +18,29 @@ targetDependencies.append(.byNameItem(name: "OpenSSL"))
 #endif
 
 let package = Package(
-    name: "SSLService",
+    name: "TLSService",
 
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SSLService",
-            targets: ["SSLService"]),
+            name: "TLSService",
+            targets: ["TLSService"]),
         ],
 
-    dependencies:
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        packageDependencies,
+    // Dependencies declare other packages that this package depends
+    dependencies: packageDependencies,
 
     targets: [
-        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SSLService",
+            name: "TLSService",
             dependencies: targetDependencies,
-            exclude: ["SSLService.xcodeproj", "README.md", "Sources/Info.plist"]),
+            exclude: ["Certs"]),
+        .testTarget(
+            name: "TLSServiceTests",
+            dependencies: targetDependencies,
+            exclude: ["Certs"]),
         ]
 )
 
